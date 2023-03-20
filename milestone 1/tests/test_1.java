3149116321e96c42e3cc39c5a91852a679985d33
AKHILMANISHJAIN <akhilj20@iitk.ac.in>
2023-03-21 03:42:11 +0530

    Array size corrected

--- milestone 1/tests/test_1.java
+++ milestone 1/tests/test_1.java
@@ -3,12 +3,12 @@
 // and prints each value to standard output.
 
 class GFG {
-	public static void main(String[] args)
+	public static void main(/*String[] args*/)
 	{
 		// declares an Array of integers.
 		int[] arr;
 
-		// allocating memory for 5 integers.
+		//allocating memory for 5 integers.
 		arr = new int[5];
 
 		// initialize the first elements of the array
@@ -21,21 +21,22 @@ class GFG {
 		arr[2] = 30;
 		arr[3] = 40;
 		arr[4] = 50;
+		//arr[2][1]=45;
 
 		// accessing the elements of the specified array
-		for (int i = 0; i < arr.length; i++)
-			System.out.println("Element at index " + i
-							+ " : " + arr[i]);
-        int [][] arr= new int[3][3];
+		// for (int i = 0; i < arr.length; i++)
+		// 	System.out.println("Element at index " + i
+		// 					+ " : " + arr[i]);
+        // int [][] arr= new int[3][3];
 
-        int arr[][]
-            = { { 2, 7, 9 }, { 3, 6, 1 }, { 7, 4, 2 } };
-        int a[][][]={{{ 2, 7, 9 }, { 3, 6, 1 }, { 7, 4, 2 } },
+        // int arr[][]
+        //     = { { 2, 7, 9 }, { 3, 6, 1 }, { 7, 4, 2 } };
+        int a[][][]={{{ 2, 7, 9}, { 3, 6, 1 }, { 7, 4, 2 } },
                         {{ 2, 7, 9 }, { 3, 6, 1 }, { 7, 4, 2 } },
-                        {{ 2, 7, 9 }, { 3, 6, 1 }, { 7, 4, 2 } }};
-
-        int max = (n1 > n2) ? n1 : n2;
+                        {{ 2, 7, 9 }, { 3, 6, 1 }, { 7, 4, 2 } },
+					};
+		//a[2][2]=arr;
+        //int max = (n1 > n2) ? n1 : n2;
         arr[0]+=30;
-
 	}
 }
