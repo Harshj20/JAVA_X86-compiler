7cb6e1b47a36e0cfa9d049b1f781f2e8759743b0
AKHILMANISHJAIN <akhilj20@iitk.ac.in>
2023-03-21 21:33:19 +0530

    Constructor support added

---
+++ milestone 1/tests/test_14.java
@@ -0,0 +1,19 @@
+//import java.io.*;
+class Geek {
+	// data members of the class.
+	int name;
+	int id;
+	Geek(int name, int id)
+	{
+		this.name = name;
+		this.id = id;
+	}
+}
+class GFG {
+	public static void main(/*String[] args*/)
+	{
+		// This would invoke the parameterized constructor.
+		Geek geek1 = new Geek(14);
+		//System.out.println("GeekName :" + geek1.name + " and GeekId :" + geek1.id);
+	}
+}
