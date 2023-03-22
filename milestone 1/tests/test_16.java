9233908ee4fa566ec39733b132c2dd6b02d68f5d
AKHILMANISHJAIN <akhilj20@iitk.ac.in>
2023-03-22 23:37:43 +0530

    While,For,Continue,Break,IfElse complete

--- milestone 1/tests/test_16.java
+++ milestone 1/tests/test_16.java
@@ -1,7 +1,21 @@
 public class test_16 {
     public static void main(String[] args) {
             int a, b, c;
-            a = 1;
-            a = b = (a > 1)?c:(a+1);
+            //b = a++;
+            // a = b = (a > 1)?c:(a+1);
+            // if(a < b){
+            //     a = 1;
+            // }
+            while(a < 3){
+                b = a + b;
+                for(;;){
+                    if(b==5){
+                        if(a==3){
+                            break;
+                        }
+                    }
+                }
+                break;
+            }
     }
 }
