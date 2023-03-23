600ec1aee99e2c80a190ca3fa6636606cb07237e
Harshj20 <hjain24in@gmail.com>
2023-03-23 19:19:50 +0530

    array almost done

--- milestone 1/tests/test_16.java
+++ milestone 1/tests/test_16.java
@@ -1,33 +1,40 @@
 public class test_16 {
-    int x = 0;
-    char y = 'c';
-    int z = 0;
+    // int x = 0;
+    // char y = 'c';
+    // int z = 0;
 
-    public int hello(int a, int b){
-        int x = 0;
-        char y = 'c';
-        int z = 0;
-        return 0;
-    }
+    // public test_16(int x){
+    //     z = x;
+    // }
+
+    // public int hello(int a, int b){
+    //     int x = 0;
+    //     char y = 'c';
+    //     int z = 0;
+    //     return 0;
+    // }
     public static void main(String[] args) {
-            int a = 0, b, c;
-            b = a++;
-            a = b = (a > 1)?c:(a+1);
-            if(a < b){
-                a = 1;
-            }
-            while(a < 3){
-                b = a + b;
-                for(;;){
-                    if(b==5){
-                        if(a==3){
-                            break;
-                        }
-                    }
-                }
-                break;
-            }
-            a = hello(2+5*5, a++);
+            // int a = 0, b, c;
+            // b = a++;
+            // a = b = (a > 1)?c:(a+1);
+            // if(a < b){
+            //     a = 1;
+            // }
+            // while(a < 3){
+            //     b = a + b;
+            //     for(;;){
+            //         if(b==5){
+            //             if(a==3){
+            //                 break;
+            //             }
+            //         }
+            //     }
+            //     break;
+            // }
+            // a = hello(2+5*5, a++);
+
+            int q[][][] = new int[10][11][12]; 
+            int b = q[4][5][1];
             // while(true){
             //     if(true)
             //         break;
