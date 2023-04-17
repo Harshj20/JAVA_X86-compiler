17bca22495317a267453bff1435eac30fc98efb3
AKHILMANISHJAIN <akhilj20@iitk.ac.in>
2023-04-17 21:53:55 +0530

    Comparison start

--- milestone3/tests/test_5.java
+++ milestone3/tests/test_5.java
@@ -30,14 +30,15 @@ class test_5
 	public static void main(String[] args)
 	{
 		// test_5(2, 3);
-		int d= 2+3*4 ;
-		int e= d*2 + d*3;
-		int z = foo(2,e);
+		//int d= 2+3*4 ;
+		//int e= d*2 + d*3;
+		//int z = foo(2,e);
+		boolean z = 4 > 3;
 		// if(e > 20){
 		// 	e = 5;
 		// }
 		// else
 		// 	e = 10;
-		System.println(z);
+		//System.println(e);
 	}
 }
